b23dd280af712c2e30e3a6fdb8750cdfcada2e21
Bongsung Bae <bsbae402@gmail.com>
2023-02-01 14:28:40 +0900

    global_settings.py and logkcs_tokens.txt have been uploaded

--- logics_pack/global_settings.py
+++ logics_pack/global_settings.py
@@ -1,2 +1,12 @@
 
 NJOBS_MULTIPROC = 8
+
+SMILES_TOKENS_PATH = "logics_pack/logics_tokens.txt"
+# GENERATOR_PRETRAIN_JSON = ASDF
+
+### following data files are provided in GitHub repo
+CHEMBL_RAW_PATH = "data/guacamol_v1_all.smiles"
+KOR_RAW_PATH = "data/data_clean_kop.csv"
+PIK3CA_RAW_PATH = "data/pubchem_5290.txt"
+
+### following files will be generated through experiments
