c8378ce373bbaa05f44e91f984a9e82a39b459c0
Bongsung Bae <bsbae402@gmail.com>
2023-02-01 15:37:28 +0900

    fixed the data path

--- logics_pack/global_settings.py
+++ logics_pack/global_settings.py
@@ -8,12 +8,12 @@ PROJECT_PATHS = {
     "SMILES_TOKENS_PATH": "logics_pack/logics_tokens.txt",
     
     ### following data files are provided in GitHub repo
-    "CHEMBL_RAW_PATH": "data/guacamol_v1_all.smiles",
-    "KOR_RAW_PATH": "data/data_clean_kop.csv",
-    "PIK3CA_RAW_PATH": "data/pubchem_5290.txt"
+    "CHEMBL_RAW_PATH": "data/chembl/guacamol_v1_all.smiles",
+    "KOR_RAW_PATH": "data/kor/data_clean_kop.csv",
+    "PIK3CA_RAW_PATH": "data/pik3ca/pubchem_5290.txt"
 }
 
-def init_project_paths(project_dir="./"):
+def build_project_paths(project_dir="./"):
     project_paths = {}
     for key, path in PROJECT_PATHS.items():
         project_paths[key] = project_dir + path
