3f117922b1ee3f068aa84968478c58883e7b34b5
Bongsung Bae <bsbae402@gmail.com>
2023-02-01 17:14:46 +0900

    global_settings.py path edit

--- logics_pack/global_settings.py
+++ logics_pack/global_settings.py
@@ -10,7 +10,12 @@ PROJECT_PATHS = {
     ### following data files are provided in GitHub repo
     "CHEMBL_RAW_PATH": "data/chembl/guacamol_v1_all.smiles",
     "KOR_RAW_PATH": "data/kor/data_clean_kop.csv",
-    "PIK3CA_RAW_PATH": "data/pik3ca/pubchem_5290.txt"
+    "PIK3CA_RAW_PATH": "data/pik3ca/pubchem_5290.txt",
+
+    ### following files will be generated through experiments
+    "CHEMBL_DATA_PATH": "data/chembl/chembl_new.smi",
+    "KOR_DATA_PATH": "data/kor/kor_affinity_new.csv",
+    "PIK3CA_DATA_PATH": "data/pik3ca/pik3ca_affinity_new.csv"
 }
 
 def build_project_paths(project_dir="./"):
@@ -29,3 +34,6 @@ KOR_RAW_PATH = "data/data_clean_kop.csv"
 PIK3CA_RAW_PATH = "data/pubchem_5290.txt"
 
 ### following files will be generated through experiments
+# CHEMBL_DATA_PATH = BASE_DIR + "data/chembl/chembl_new.smi"
+# KOR_DATA_PATH = BASE_DIR + "data/kor/kor_affinity_new.csv"
+# PIK3CA_DATA_PATH = BASE_DIR + "data/pik3ca/pik3ca_affinity_new.csv"
