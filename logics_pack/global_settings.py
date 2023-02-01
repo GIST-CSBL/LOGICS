d3bbe3439281d8f934b36fcdf94c05dedbfd4729
Bongsung Bae <bsbae402@gmail.com>
2023-02-01 15:20:16 +0900

    global_settings.py, I trying to change the way to call the project file paths

--- logics_pack/global_settings.py
+++ logics_pack/global_settings.py
@@ -1,5 +1,22 @@
 
+class Object:
+    pass
+
 NJOBS_MULTIPROC = 8
+
+project_paths = {
+    "SMILES_TOKENS_PATH": "logics_pack/logics_tokens.txt",
+    
+    ### following data files are provided in GitHub repo
+    "CHEMBL_RAW_PATH": "data/guacamol_v1_all.smiles",
+    "KOR_RAW_PATH": "data/data_clean_kop.csv",
+    "PIK3CA_RAW_PATH": "data/pubchem_5290.txt"
+}
+
+def init_project_paths(project_dir="./"):
+    for key, path in project_paths.items():
+        project_paths[key] = project_dir + path
+
 
 SMILES_TOKENS_PATH = "logics_pack/logics_tokens.txt"
 # GENERATOR_PRETRAIN_JSON = ASDF
