4fde978ff4916651dfada782b8c7622e55a43b22
Bongsung Bae <bsbae402@gmail.com>
2023-02-02 16:19:47 +0900

    pre-training data path has beend added

--- logics_pack/global_settings.py
+++ logics_pack/global_settings.py
@@ -26,6 +26,9 @@ PROJECT_PATHS = {
     "PIK3CA_FOLD_JSON": "data/pik3ca/pik3ca_fold_splits.json",
     "KOR_DATA_FP": "data/kor/kor_aff_npfps.npy",
     "PIK3CA_DATA_FP": "data/pik3ca/pik3ca_aff_npfps.npy",
+
+    ## pre-training phase
+    "PRETRAINING_DATA_PATH": "data/chembl/pre-training.smi"
 }
 
 def build_project_paths(project_dir="./"):
@@ -52,17 +55,3 @@ class ExperimentSettings:
 
     def get_setting(self, key):
         return self.expset[key]
-
-
-SMILES_TOKENS_PATH = "logics_pack/logics_tokens.txt"
-# GENERATOR_PRETRAIN_JSON = ASDF
-
-### following data files are provided in GitHub repo
-CHEMBL_RAW_PATH = "data/guacamol_v1_all.smiles"
-KOR_RAW_PATH = "data/data_clean_kop.csv"
-PIK3CA_RAW_PATH = "data/pubchem_5290.txt"
-
-### following files will be generated through experiments
-# CHEMBL_DATA_PATH = BASE_DIR + "data/chembl/chembl_new.smi"
-# KOR_DATA_PATH = BASE_DIR + "data/kor/kor_affinity_new.csv"
-# PIK3CA_DATA_PATH = BASE_DIR + "data/pik3ca/pik3ca_affinity_new.csv"
