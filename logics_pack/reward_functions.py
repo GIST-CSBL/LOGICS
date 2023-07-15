71e413bdc8bb28b1d599137593dddbc029454350
Bongsung Bae <bsbae402@gmail.com>
2023-07-15 23:50:04 +0900

    upload new class for reward param search

--- logics_pack/reward_functions.py
+++ logics_pack/reward_functions.py
@@ -16,3 +16,11 @@ def pAff_to_reward_t2(pAff):
         It should be in numpy 1-d array.
     """
     return np.tanh((pAff-6.8)*0.3)
+
+class AffinityRewardTransform:
+    def __init__(self, alpha=0.3, beta=6.5):
+        self.alpha = alpha
+        self.beta = beta
+    
+    def pAff_to_reward(self, pAff):
+        return np.tanh((pAff-self.beta)*self.alpha)
